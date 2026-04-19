0fc1e5ac04f863978958007eb0573f49b82ce686
youabd123 <yousuf.hh@icloud.com>
2026-04-19 09:01:43 +0200

    Add project files.

---
+++ CleanArchitectureApi.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureApi.Domain.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+}
